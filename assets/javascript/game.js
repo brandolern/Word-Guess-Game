db533d4e54c3eb0318c566331760718f85d2fa31
brandolern <46857695+brandolern@users.noreply.github.com>
2019-02-22 22:23:07 -0700

    game logic fininshed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,91 @@
+//Word Guess Game
+
+//Wins, guesses, and x variables
 var wins = 0;
 var guessesRemaining = 13;
-var lettersGuessed = [];
-var placeholder = "_ ";
-
-var bands = ["STRING1", "STRING2", "OTHER-STRING", "LAST-STRING"];
-
 var x = 0;
+
+//Arrays
+var bandLetters = [];
+var lettersGuessed = [];
+var correctLetters = [];
+var bands = ["STRING1", "HELLO", "OTHER-STRING", "LAST-STRING"];
+
+//Selects current band that user is guessing
 var currentBand = bands[x];
 
+
+// Sets correctLetters array equal to "_", for the length of currentBand
+var createPlaceholder = function () {
+    for (i = 0; i < currentBand.length; i++) {
+        correctLetters[i] = "_";
+    }
+}
+createPlaceholder();
+//Grabs containers from HTML
 var winsText = document.getElementById("wins");
 var placeholderText = document.getElementById("placeholder");
 var guessesRemainingText = document.getElementById("guessesRemaining");
 var lettersGuessedText = document.getElementById("lettersGuessed");
 
+//Displays content inside containers
 var pageContent = function () {
     winsText.textContent = wins;
-    placeholderText.textContent = placeholder.repeat(currentBand.length);
+    placeholderText.textContent = correctLetters.join(" ");
     guessesRemainingText.textContent = guessesRemaining;
     lettersGuessedText.textContent = lettersGuessed;
 }
 
-pageContent();
+//Splits the current value of currentBand into seperate letters
+var splitBand = function () {
+    bandLetters = currentBand.split("");
+}
 
-var changeWord = function () {
-    var x = 1;
-
+//Change to the next band
+var nextWord = function () {
+    x++;
+    guessesRemaining = 13;
+    currentBand = bands[x];
+    lettersGuessed = [];
+    correctLetters = [];
+    createPlaceholder();
+    splitBand();
 };
+
+
+splitBand();
+pageContent();
 
 document.onkeyup = function (event) {
 
     var letter = event.key.toUpperCase();
 
 
+
+    for (i = 0; i < bandLetters.length; i++) {
+        if (bandLetters[i] === letter) {
+
+            correctLetters[i] = bandLetters[i];
+        }
+    }
+
     if (lettersGuessed.indexOf(" " + letter) === -1) {
         lettersGuessed.push(" " + letter);
-        console.log(letter)
-        console.log(lettersGuessed)
     }
+
+    if (bandLetters.indexOf(letter) === -1) {
+        guessesRemaining--;
+    }
+
+    if (correctLetters.indexOf("_") === -1) {
+        wins++;
+        nextWord();
+    }
+
+    if (guessesRemaining === 0) {
+        nextWord();
+    }
+
 
     pageContent();
 
@@ -44,4 +93,3 @@ document.onkeyup = function (event) {
 
 
 //Create function that will switch picture and word when user loses or wins
-//Create function that will capture user key inputs
